3ee89a6a66a3c3e468a3a5df58758f0bdda72fac
Justin Halsall <Juice10@users.noreply.github.com>
2023-06-09 15:27:52 +0200

    Rename `assetCaptureConfig` to `assetCapture`

--- packages/rrweb/src/record/index.ts
+++ packages/rrweb/src/record/index.ts
@@ -90,7 +90,7 @@ function record<T = eventWithTime>(
     userTriggeredOnInput = false,
     collectFonts = false,
     inlineImages = false,
-    assetCaptureConfig = {
+    assetCapture = {
       captureObjectURLs: true,
       captureOrigins: false,
     },
@@ -337,7 +337,7 @@ function record<T = eventWithTime>(
   assetManager = new AssetManager({
     mutationCb: wrappedAssetEmit,
     win: window,
-    assetCaptureConfig,
+    assetCapture,
   });
 
   const shadowDomManager = new ShadowDomManager({
